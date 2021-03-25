5b282283bf6f4fa768ebe9468ac79374ce727dc1
sudheer panangipalli <pvnssudheer@msitprogram.net>
2021-03-25 12:21:56 +0530

    Demo 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World Good to see you!'));
+app.get('/', (req, res) => res.send('Good Day '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
